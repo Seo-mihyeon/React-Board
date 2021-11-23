3fdec72bb9318a888e8fb08347e215e91c7eac02
algus8615 <algus8615@naver.com>
2021-11-23 11:14:25 +0900

    loading view api add

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import TableHead from '@material-ui/core/TableHead'
 import TableBody from '@material-ui/core/TableHead'
 import TableRow from '@material-ui/core/TableRow'
 import TableCell from '@material-ui/core/TableCell'
+import { CircularProgress } from '@material-ui/core';
 import { mergeClasses, withStyles } from '@material-ui/styles';
 import { render } from '@testing-library/react';
 import { Component } from 'react';
@@ -13,22 +14,46 @@ import { Component } from 'react';
 const styles = theme => ({
   root : {
     width : '100%',
+    marginTop: 30,
     overflowX : "auto"
   },
   table: {
     minWidth : 1080
+  },
+  progress : {
+    margin: 20
   }
 })
+
+/*
+  실행 순서
+  1) constructor()
+
+  2) componentWillMount()
+
+  3) render()
+
+  4) componentDidMount()
+
+ */
+/*
+props or state 가 변경시 => shouldComponentUpdate()
+
+ */
+
+
 
 // 변경가능 state 변경불가능 props
 class App extends Component{
 
   state = {
-    customers : ""
+    customers : "",
+    completed: 0
   }
 
   // api서버에 접근하여 데이터를 받아오는 작업
   componentDidMount(){
+    this.timer = setInterval(this.progress, 20);  //0.02초마다 수행
     this.callApi()
     .then(res => this.setState({customers: res}))
     .catch(err => console.log(err));
@@ -38,6 +63,11 @@ class App extends Component{
     const response = await fetch('/api/customers'); // 접속하고자하는 api 주소
     const body = await response.json(); // 목록을 json 형태로 만들어서 넣는다
     return body;
+  }
+
+  progress = () =>{
+    const {completed} = this.state;
+    this.setState({completed : completed >=100 ? 0 : completed + 1});
   }
 
   render() {
@@ -58,7 +88,13 @@ class App extends Component{
           <TableBody>
             {this.state.customers ? this.state.customers.map( c => { 
               return ( <Customer key={c.id} id={c.id} image = {c.image} name = {c.name} birthday = {c.birthday} gender = {c.gender} job ={c.job} /> ); 
-            }) : ""}
+            }) : 
+            <TableRow>
+              <TableCell colSpan="6" align="center">
+                <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
+              </TableCell>
+            </TableRow>
+            }
           </TableBody>
         </Table>
       </Paper>
